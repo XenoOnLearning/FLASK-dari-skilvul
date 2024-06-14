88bd002c42b4151b96ebb2bf92073511abe2f479
XenoOnLearning <Farrellpambudi0@gmail.com>
2024-06-14 23:49:22 +0700

    Makasih

--- app/app.py
+++ app/app.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def entry_point():
     return "Index page"
 
@@ -13,15 +13,16 @@ def hello():
 @app.route("/login", methods= ["GET", "POST"])
 def login():
     if request.method == "POST":
-        return "Ga bisa"()
+        return "bisa"
     else:
-        return "Eh malah tambah gabisa"()
-@app.route("/sensor/data", methods="POST")
+        return "Eh malah tambah gabisa"
+@app.route("/sensor/data", methods=["POST"])
 def sensor():
     return {
         "temperature": 30,
         "kelembapan": 1,
         "timestamp":"09:09:30"
     }
+
 if __name__ == '__main__':
     app.run(debug=True)
--- app/main_fastapi.py
+++ app/main_fastapi.py
@@ -3,16 +3,17 @@ import uvicorn
 
 app = FastAPI()
 
-@app.get("/")
+@app.get('/')
 def root():
     return "hello  world"
 
-@app.post("/sensor/data")
+@app.post("sensor/data/")
 def sensor():
     return {
         "temperature": 30,
         "kelembapan": 1,
-        "timestamp":"09:09:30"
+        "timestamp":"09:09:30",
     }
+
 if__name__ == "__main__"
 uvicorn.run(app, host="0.0.0.0", port=8000)
